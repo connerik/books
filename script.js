f71feca876d67da4906364f2283c05499d4c65c1
connerik <connerik@msu.edu>
2026-02-26 02:50:26 +0000

    cleaned up

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js"
-
 const supabaseUrl = 'https://behktqcgvgrsrdklkhkg.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlaGt0cWNndmdyc3Jka2xraGtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NzExNzMsImV4cCI6MjA4NzQ0NzE3M30.UoWwqh_SgZ2pN3LGNb0ozyuRLwANtLoCcITUS2M38Rg'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+
+
 async function getBooks() {
-  const { data: books, error } = await supabase
-    .from('books')
-    .select('*')
-
-  if (error) {
-    console.error(error)
-    return
-  }
-
-  const bookList = document.getElementById('books')
-
+  let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+  
   for (let book of books) {
-    const row = document.createElement("tr")
-
-    row.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.isbn}</td>
-    `
-
-    bookList.appendChild(row)
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td></tr>`;
   }
 }
 
-getBooks()
+getBooks();
